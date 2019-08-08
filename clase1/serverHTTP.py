af90d967f253a86ddbcddb870d6ec41750c89c7d
Joaquin Cruz D <joacocruz6@gmail.com>
2019-08-08 17:38:51 -0400

    Comenzando el proxy

--- clase1/serverHTTP.py
+++ clase1/serverHTTP.py
@@ -1,3 +1,5 @@
+# donde esta el proxy http://localhost:8888 
+# donde correrlo http://anakena.dcc.uchile.cl:8989/secret/
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 import sys
 import json
@@ -7,9 +9,16 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         configFile = sys.argv[1]
         with open(configFile,'r') as f:
             configurations = json.load(f)
-            self.send_response(200) # Envio el status de respuesta
             # Aca se envian los headers, 
-            usuario = configurations['X-ElQuePregunta']
+            usuario = configurations['user']
+            blocked_paths = configurations['blocked']
+            print(self.path)
+            print(self.request)
+            if self.path in blocked_paths:
+                self.send_response(403)
+                self.send_error(403)
+            else:
+                self.send_response(200) # Envio el status de respuesta
             self.send_header("X-ElQuePregunta",usuario)
             self.end_headers()
         return
