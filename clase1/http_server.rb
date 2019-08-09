3d40fecf5ae2260274ec1df708739bab12c2dee8
Joaquin Cruz D <joacocruz6@gmail.com>
2019-08-09 15:52:52 -0400

    Terminado proxy

--- clase1/http_server.rb
+++ clase1/http_server.rb
@@ -1,5 +1,6 @@
 require 'socket' # necesito la gema de sockets standard
 require 'json'
+require 'uri'
 def replace_forbidden(forbidden_words,text)
     return 
 end
@@ -9,23 +10,62 @@ def run(json_file)
         Thread.start(proxy.accept) do |client|
             file = File.read(json_file)
             json = JSON.parse(file)
-            request = client.gets
-            puts request
-            info_request = request.split(" ")
-            url = info_request[1]
-            if json['blocked'].include? url.chop
+            puts json['forbidden_words']
+            request= client.readline
+            STDERR.puts request
+            verb    = request[/^\w+/]
+            url     = request[/^\w+\s+(\S+)/, 1]
+            version = request[/HTTP\/(1\.\d)\s*$/, 1]
+            uri     = URI::parse url
+            user = json['user']
+            # Show what got requested
+            puts((" %4s "%verb) + url)
+            if json['blocked'].include? url
                 client.print "HTTP/1.1 403\r\n"
                 client.print "\r\n"
                 client.print "Forbidden"
             else
-                host = url.match(/www.([a-z]+).(cl|com)\//)
-                puts host[0].chop
-                server = TCPSocket.open(host[0].chop,80)
-                client.print "HTTP/1.1 200\r\n"
-                client.print "user: #{json['user']}\r\n"
-                client.print "Content-Type: text/html\r\n"
-                client.print "\r\n"
-                client.print "Hello World!"
+                to_server = TCPSocket.new(uri.host, (uri.port.nil? ? 80 : uri.port))
+                to_server.write("#{verb} #{uri.path}?#{uri.query} HTTP/#{version}\r\n")
+                to_server.write("X-Elquepregunta: #{user}\r\n")
+                content_length = 0
+                loop do
+                    line = client.readline # Tomo el resto de la peticion del cliente
+                    if line =~ /^Content-Length:\s+(\d+)\s*$/
+                        content_length = $1.to_i
+                    end
+                    if line =~ /^proxy/i
+                        next
+                      elsif line.strip.empty?
+                        to_server.write("Connection: close\r\n\r\n")
+                        
+                        if content_length>= 0
+                          to_server.write(client.read(content_length))
+                        end
+                        
+                        break
+                      else
+                        to_server.write(line)
+                      end
+                end
+                # Aca debo modificar para enviar el header por el que pregunta
+                buff = ""
+                loop do
+                to_server.read(4048, buff)
+                package = buff.split("\r\n\r\n")
+                header = package[0]
+                data = package[1]
+                for x in json['forbidden_words']
+                    for key in x.keys
+                        data.gsub! key,x[key]
+                    end
+                end
+                buff = header +"\r\n\r\n"+ data
+                puts buff
+                client.write(buff)
+                break if buff.size < 4048
+                end
+                to_server.close()
             end
             client.close()
         end
